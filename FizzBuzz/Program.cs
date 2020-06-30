2f589e080df87efcef8ba43ab188180438599243
WrightS95 <60717800+WrightS95@users.noreply.github.com>
2020-06-30 13:50:57 -0500

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++) 
+            {                                    
+                if(num % 3 == 0 && num % 5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz"); //number is multiple of 3 and 5, prints fizzbuzz
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz"); //number is multiple of 3, prints fizz
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz"); //number is multiple of 5, prints buzz
+                }
+                else
+                {
+                    Console.WriteLine($"{num}"); //prints numbers from 1 to 100
+                }
+            }
+        }
+    }
+}
